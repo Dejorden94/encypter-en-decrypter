de731588249a4e3298deb6ff44e3c3e62a4a94ab
Dejorden <d.moerman@hotmail.nl>
2023-08-30 16:12:19 +0200

    add key in js

--- js/main.js
+++ js/main.js
@@ -1,92 +1,9 @@
-const encryptInput = document.querySelector("#js--encrypt");
-
 function encrypt(messageToEncrypt) {
     let messageToEncryptLowerCase = messageToEncrypt.toLowerCase()
     let encryptedMessage = "";
+    const possibleResults = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
     for (let i = 0; i < messageToEncrypt.length; i++) {
-        switch (messageToEncryptLowerCase[i]) {
-            case "a":
-                encryptedMessage = encryptedMessage + "c";
-                break;
-            case "b":
-                encryptedMessage = encryptedMessage + "d";
-                break;
-            case "c":
-                encryptedMessage = encryptedMessage + "e";
-                break;
-            case "d":
-                encryptedMessage = encryptedMessage + "f";
-                break;
-            case "e":
-                encryptedMessage = encryptedMessage + "g";
-                break;
-            case "f":
-                encryptedMessage = encryptedMessage + "h";
-                break;
-            case "g":
-                encryptedMessage = encryptedMessage + "i";
-                break;
-            case "h":
-                encryptedMessage = encryptedMessage + "j";
-                break;
-            case "i":
-                encryptedMessage = encryptedMessage + "k";
-                break;
-            case "j":
-                encryptedMessage = encryptedMessage + "l";
-                break;
-            case "k":
-                encryptedMessage = encryptedMessage + "m";
-                break;
-            case "l":
-                encryptedMessage = encryptedMessage + "n";
-                break;
-            case "m":
-                encryptedMessage = encryptedMessage + "o";
-                break;
-            case "n":
-                encryptedMessage = encryptedMessage + "p";
-                break;
-            case "o":
-                encryptedMessage = encryptedMessage + "q";
-                break;
-            case "p":
-                encryptedMessage = encryptedMessage + "r";
-                break;
-            case "q":
-                encryptedMessage = encryptedMessage + "s";
-                break;
-            case "r":
-                encryptedMessage = encryptedMessage + "t";
-                break;
-            case "s":
-                encryptedMessage = encryptedMessage + "u";
-                break;
-            case "t":
-                encryptedMessage = encryptedMessage + "v";
-                break;
-            case "u":
-                encryptedMessage = encryptedMessage + "w";
-                break;
-            case "v":
-                encryptedMessage = encryptedMessage + "x";
-                break;
-            case "w":
-                encryptedMessage = encryptedMessage + "y";
-                break;
-            case "x":
-                encryptedMessage = encryptedMessage + "z";
-                break;
-            case "y":
-                encryptedMessage = encryptedMessage + "a";
-                break;
-            case "z":
-                encryptedMessage = encryptedMessage + "b";
-                break;
-            default:
-                //Als het karakter niet iets is uit he bovenstaande, dan is het het karakter wat het als was.
-                encryptedMessage = encryptedMessage + messageToEncryptLowerCase[i];
-        }
+
     }
     return encryptedMessage;
 }
@@ -183,12 +100,14 @@ function decrypt(messageToDecrypt) {
     return decryptMessage;
 }
 
+const encryptInput = document.querySelector("#js--encrypt");
 encryptInput.addEventListener("keydown", function (event) {
     if (event.key === "Enter") {
         this.value = encrypt(this.value);
         this.setAttribute("disabled", "");
+        console.log(keyInput.value);
     }
-})
+});
 
 const decryptInput = document.querySelector("#js--decrypt");
 decryptInput.addEventListener("keydown", function (event) {
@@ -196,4 +115,6 @@ decryptInput.addEventListener("keydown", function (event) {
         this.value = decrypt(this.value);
         this.setAttribute("disabled", "");
     }
-})
+});
+
+const keyInput = document.querySelector("#js--key");
